b80947b7f341b6036a7ef741328c5f9ab1643fe4
Mats O. Stangjordet <Matso.Kyrie@gmail.com>
2026-08-10 17:46:34 +0200

    feat: add basic HTTP health check

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from time import perf_counter
+import httpx
+
+def check_url(url):
+    start_time = perf_counter()
+
+    response = httpx.get(url)
+
+    elapsed_time = perf_counter() - start_time
+
+    print("URL:", url)
+    print("Status:", response.status_code)
+    print("Reponse time:", elapsed_time)
+
 def main():
-    print("Hello from platform-engineering-lab!")
+    check_url("https://example.com")
+
 
 
 if __name__ == "__main__":
