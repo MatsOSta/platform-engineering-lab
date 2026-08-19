12064a9f4b7049f8281df5421d491b06858af26f
Mats O. Stangjordet <112391018+MatsOSta@users.noreply.github.com>
2026-08-19 12:20:50 +0200

    feat(ci): add IaC security baseline scan (#15)
    * feat(ci): add IaC security baseline scan
    
    * fix(github): require signed commits
    
    * feat(ci): enforce IaC security scan
    
    * feat(github): require IaC security check

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,15 +14,17 @@ provider "github" {
 }
 
 resource "github_branch_protection" "master" {
-  repository_id  = "platform-engineering-lab"
-  pattern        = "master"
-  enforce_admins = false
+  repository_id          = "platform-engineering-lab"
+  pattern                = "master"
+  enforce_admins         = false
+  require_signed_commits = true
 
   required_status_checks {
     strict = true
     contexts = [
       "checks",
       "container-scan",
+      "iac-security",
       "opentofu-validation",
     ]
   }
